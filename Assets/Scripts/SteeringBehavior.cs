71297d43ff76e220d8998a0e851104a5424fee6f
Bigg215 <glenn.j.gilroy@gmail.com>
2019-01-23 04:06:37 -0500

    Added Seek/Flee Control Behavior

--- Assets/Scripts/SteeringBehavior.cs
+++ Assets/Scripts/SteeringBehavior.cs
@@ -18,20 +18,35 @@ public class SteeringBehavior : MonoBehaviour
 
     private void Update()
     {
-        Vector3 desiredVelocity = target.transform.position - transform.position;
-        desiredVelocity = desiredVelocity.normalized * MaxVelocity;
+        this.Steering(this.Flee(target));
+    }
 
+    public Vector3 Seek(Transform t)
+    {
+        Vector3 desiredVelocity = t.position - transform.position;
+        desiredVelocity = desiredVelocity.normalized * MaxVelocity;
+        return desiredVelocity;
+    }
+
+    public Vector3 Flee(Transform t)
+    {
+        Vector3 desiredVelocity = transform.position - t.position;
+        desiredVelocity = desiredVelocity.normalized * MaxVelocity;
+        return desiredVelocity;
+    }
+
+    public void Steering(Vector3 desiredVelocity)
+    {
         Vector3 steering = desiredVelocity - velocity;
         steering = Vector3.ClampMagnitude(steering, MaxForce);
         steering /= Mass;
 
         velocity = Vector3.ClampMagnitude(velocity + steering, MaxVelocity);
-        
+
         transform.position += velocity * Time.deltaTime;
         transform.right = velocity.normalized;
 
         Debug.DrawRay(transform.position, velocity.normalized * 2, Color.green);
         Debug.DrawRay(transform.position, desiredVelocity.normalized * 2, Color.magenta);
-
     }
 }
